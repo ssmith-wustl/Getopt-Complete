bed7ef675e335797e50931b9bf65b4aca97bc66e
Nathan Nutter <iam@nnutter.com>
2011-08-23 16:22:25 -0500

    add example of .bashrc completion handler

--- bin/getopt_complete.sh
+++ bin/getopt_complete.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# You need to add a completion handler to Bash in your .bashrc, e.g.:
+#   complete -F _getopt_complete your_cmd
 function _getopt_complete () {
     COMPREPLY=($( COMP_CWORD=$COMP_CWORD perl `which ${COMP_WORDS[0]}` ${COMP_WORDS[@]:0} ));
 }
